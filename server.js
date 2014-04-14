d8418ffb82255f54d8588693277309e4122c0740
BogdanZzz <ikrinka24@yandex.ru>
2014-04-14 08:10:41 -0700

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http'),
-    port = process.env.PORT || 8001;
- 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(parseInt(port));
